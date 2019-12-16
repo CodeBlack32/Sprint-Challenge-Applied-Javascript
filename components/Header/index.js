1984b79b78c9f4181785fa2d32a26f844dc57cbc
Jarvis Black <usnblack@gmail.com>
2019-12-15 21:48:43 -0500

    so far

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,6 +8,28 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
-//Start
+//Start 
 
-function Header() {}
+
+function Header() {
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    const headerContain = document.querySelector('.header-container');
+
+    // Appends
+    headerContain.appendChild(header);
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temp);
+
+    //Class 
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+
+    return Header
+}
